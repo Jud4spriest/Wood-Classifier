0f12491a36419f71a71e6c4d5582141aa9391ba4
Marcos Azevedo <mazevedo.snts@gmail.com>
2022-03-04 07:57:31 -0300

    upload do projeto para o repositório

---
+++ classificacao_script.py
@@ -0,0 +1,66 @@
+import cv2 as cv
+import maior
+import matplotlib.pyplot as plt
+
+
+
+plt.style.use('seaborn-dark')
+
+def classifica(fframe,color):
+
+    output = cv.connectedComponentsWithStats(fframe,4,cv.CV_32S)
+    stats = output[2]
+    stats=stats[1:len(stats)]
+    altura, largura = fframe.shape
+    nnos = output[0]-1
+
+    print(stats)
+    print('largura do maior nó = ' + str(maior.maiorlargura(stats)) + ' ||| altura do maior nó + ' + str(maior.maioraltura(stats)))
+    print('Número de nós : ' + str(nnos))
+
+    if output[0] ==1:
+        print('Capa tipo A')
+        tipo = 'A'
+
+    elif 1 < output[0] < 12 and maior.maiorlargura(stats)< 250 and maior.maioraltura(stats)<200:
+        print('Capa tipo B')
+        tipo = 'B'
+    else:
+        print('Capa tipo C')
+        tipo = 'C'
+
+
+
+    # Gerando gráficos
+
+    plt.scatter(stats[:,3],stats[:,2])
+    plt.xlabel('Altura do nó')
+    plt.ylabel('Largura do nó')
+    plt.title('Dispersão da dimensão dos nós')
+    plt.savefig('scatter.png')
+    scatter =cv.imread('scatter.png')
+    plt.close()
+
+
+    plt.hist(stats[:,4])
+    plt.xlabel('Área do nó')
+    plt.ylabel('Frequência')
+    plt.title('Dispersão da dimensão dos nós')
+    plt.savefig('hist.png')
+    hist = cv.imread('hist.png')
+
+
+
+
+    for rows in stats:
+        color = cv.rectangle(color, (rows[0], rows[1]), (rows[0] + rows[2], rows[1] + rows[3]), (255, 0, 0), 2)
+
+
+    return nnos,tipo,color,scatter,hist
+
+
+
+
+
+
+
---
+++ tratament_preliminar.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+def tratamento_preliminar(fframe,x,y,w,h):
+    fframe = fframe[x:x + w, y:y + h]
+    fframe = cv.cvtColor(fframe, cv.COLOR_BGR2GRAY)
+    tresh,binaria = cv.threshold(fframe, 175, 255, cv.THRESH_BINARY)
+    binaria = ~binaria
+    img_erosion = cv.erode(binaria, cv.getStructuringElement(cv.MORPH_ELLIPSE, (2, 2)), iterations=1)
+    img_dilation = cv.dilate(img_erosion, cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3)), iterations=3)
+    pronta = cv.morphologyEx(img_dilation, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5)))
+   # pronta = ~pronta
+
+    return pronta
+
+
+
+
