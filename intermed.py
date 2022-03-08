b3e3bba29e9ee1c157148c80076774e2f3a013c0
Marcos Azevedo <mazevedo.snts@gmail.com>
2022-03-08 09:12:21 -0300

    Função salva imagens atualizada

--- intermed.py
+++ intermed.py
@@ -14,10 +14,10 @@ def intermediario(imagem_a_classificar,x,y,w,h):
     """Retorno Novo"""
     [numero_de_nos,tipo_norma,colorida_bounding_box,scatter,hist] = classificacao_script.classifica(pb, imagem_a_classificar)   #Alterar o retorno do classifica
 
-    # cv2.imwrite(dir+'scatter.png',scatter)
-    # cv2.imwrite(dir+'hist.png',hist)
-    # cv2.imwrite(dir+'color.png',colorida_bounding_box)
-    # cv2.imwrite(dir+'pb.png',pb)
+    cv2.imwrite(dir+'scatter.png',scatter)
+    cv2.imwrite(dir+'hist.png',hist)
+    cv2.imwrite(dir+'color.png',colorida_bounding_box)
+    cv2.imwrite(dir+'pb.png',pb)
 
     return numero_de_nos, tipo_norma, colorida_bounding_box, pb, scatter, hist
 
